e4710d414c8533bb0f5f8f5ccb8e6cafd9b5219c
Ben Siebert <hello@ben-siebert.de>
2024-08-24 16:26:15 +0200

    update

---
+++ preview.js
@@ -0,0 +1,30 @@
+/**
+ * /preview.js
+ *
+ * Author: Ben Siebert <hello@ben-siebert.de>
+ * Copyright: Copyright (c) 2018-2024 Ben Siebert. All rights reserved.
+ * License: Project License
+ * Created At: 24.08.2024
+ *
+ */
+
+const { execSync, spawn } = require('child_process');
+const path = require('path');
+
+function runTex() {
+    execSync("pdflatex -output-directory ../pdf -job-name CodeUp main.tex", {
+        cwd: path.join(__dirname, "src"),
+        stdio: "inherit"
+    })
+}
+function runBiber() {
+    execSync("biber ../pdf/CodeUp", {
+        cwd: path.join(__dirname, "src"),
+        stdio: "inherit"
+    })
+}
+
+runTex();
+runTex();
+runBiber();
+runTex();
